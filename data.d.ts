71712b03abe60a30901ea0d6d0f6f8ad67d3e329
Bartek <bartekk.jonca@gmail.com>
2021-08-08 13:36:03 +0200

    type definition added

---
+++ data.d.ts
@@ -0,0 +1,15 @@
+declare module 'polskie-miejscowosci' {
+    const type: [
+      {
+        Commune: string
+        District: string
+        Id: string
+        Latitude: number
+        Longitude: number
+        Name: string
+        Province: string
+        Type: "village" | "city"
+      }
+    ]
+    export default type
+  }
